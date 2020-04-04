056e7fdfc3febf26acd4da40d5f952e22b152219
fernandocoba <fernandocoba@gmail.com>
2020-04-04 17:05:26 -0300

    first commit 04-04

--- src/main/java/com/springboot/microservice/microservices_item/MicroserviceItemApplication.java
+++ src/main/java/com/springboot/microservice/microservices_item/MicroserviceItemApplication.java
@@ -2,7 +2,11 @@ package com.springboot.microservice.microservices_item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCircuitBreaker
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceItemApplication {
 
--- src/main/java/com/springboot/microservice/microservices_item/services/ItemService.java
+++ src/main/java/com/springboot/microservice/microservices_item/services/ItemService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.springboot.microservice.microservices_item.dto.RestTemplateDto;
 import com.springboot.microservice.microservices_item.entities.Item;
 import com.springboot.microservice.microservices_item.entities.Product;
+import com.springboot.microservice.microservices_item.response.Response;
 import com.springboot.microservice.microservices_item.response.ResponseRestTemplate;
 
 @Service
@@ -17,6 +20,7 @@ public class ItemService {
 	@Autowired
 	RestTemplateDto restTemplateDto;
 	
+	@HystrixCommand(defaultFallback = "metodoAlternativo")
 	public List<Item> obtainInformationAllItems() {
 
 		List<ResponseRestTemplate> products = restTemplateDto.obtainInformationAllProducts();
@@ -32,7 +36,11 @@ public class ItemService {
 
 		return productsList.stream().map(pr -> new Item(pr, 0)).collect(Collectors.toList());
 	}
-
+	
+	public Response metodoAlternativo() {
+		return new Response(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()) , "Error Irrecuperable", null, null);
+	}
+	
 	public Item obtainInformationItemById(Long productId, Integer quantity) {
 		ResponseRestTemplate product = restTemplateDto.obtainInformationByProductId(productId);
 		return new Item(product.getProduct(), quantity);
